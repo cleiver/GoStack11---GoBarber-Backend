e328aeeff14d6716f315cdaa47d986d4c56c339d
cleiver <cleiver@gmail.com>
2020-04-14 18:32:06 -0300

    ✏️ Fixing error throwing

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -17,7 +17,7 @@ class CreateUserService {
     });
 
     if (checkEmailExists) {
-      throw Error('Email address already used.');
+      throw new Error('Email address already used.');
     }
 
     const hashedPassword = await hash(password, 8);
