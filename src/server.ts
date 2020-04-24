dc5c3d727d5384825e2bc5078bc6c633a54f8ddc
cleiver <cleiver@gmail.com>
2020-04-24 10:48:55 -0300

    ➕  Installing cors

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import uploadConfig from './config/upload';
-import routes from './routes';
+import cors from 'cors';
 
+import uploadConfig from './config/upload';
 import './database';
+
 import AppError from './errors/AppError';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
