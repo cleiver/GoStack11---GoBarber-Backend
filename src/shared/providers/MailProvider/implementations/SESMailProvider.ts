6811a926a7e0477f303e5507444922cdc0cf3497
Cleiver Carvalhal Carneiro <cleiver@gmail.com>
2020-05-17 20:40:16 -0300

    ✨ Creating a mail provider pro production

---
+++ src/shared/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,47 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import aws from 'aws-sdk';
+import { injectable, inject } from 'tsyringe';
+
+import IMailTemplateProvider from '@shared/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import mailConfig from '@config/mail';
+import IMailProvider from '../models/IMailprovider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+
+@injectable()
+export default class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: process.env.AWS_DEFAULT_REGION,
+      }),
+    });
+  }
+
+  public async sendMail({
+    to,
+    from,
+    subject,
+    template,
+  }: ISendMailDTO): Promise<void> {
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(template),
+    });
+  }
+}
